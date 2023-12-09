44458077eefa569b6a726a3c3b68819b907a3135
Anton Makushenko <antonmakushenko@iMac-Anton.local>
2023-12-09 04:36:26 +0200

    setup vue app, add tailwindcss, add header

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from '@/components/Header/Header.vue'
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderLinkList from './HeaderLinkList/HeaderLinkList.vue'
+</script>
+
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10" />
+      <div>
+        <h2 class="text-xl uppercase font-bold">vue sneakers</h2>
+        <p class="text-slate-400">Магазин лучших кросовок</p>
+      </div>
+    </div>
+    <HeaderLinkList />
+  </header>
+</template>
